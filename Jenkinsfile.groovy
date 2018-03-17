044d6c1f102f3f66be360f66a2f3e31e1ae644c8
leonardo.park <blue.park@kt.com>
2018-03-17 22:26:13 +0900

    add deploy script

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,48 +1,47 @@
 #!groovy
 node {
-
-    def app
     def git
-
     def commitHash
 
-    stage('Checkout') {
-        git = checkout scm
-        commitHash = git.GIT_COMMIT
+    ansiColor("xterm") {
 
-        println(git)
-    }
-
-    stage('Test') {
-        sh './gradlew test || true'
-    }
-
-    stage('Build') {
-        try {
-            sh './gradlew build -x test'
-        } catch(e) {
-            mail subject: "Jenkins Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) failed with ${e.message}",
-                to: 'codingman@outlook.kr',
-                body: "Please go to $env.BUILD_URL."
+        stage('Checkout') {
+            git = checkout scm
+            commitHash = git.GIT_COMMIT
         }
-    }
 
-    stage('Build Docker Image') {
-        app = docker.build("getintodevops/hellonode")
-    }
+        stage('Test') {
+            sh './gradlew test || true'
+        }
 
-    stage('Archive') {
-        parallel (
-
-            "Archive Artifacts" : {
-                archiveArtifacts artifacts: '**/build/libs/*.jar', fingerprint: true
-            },
-
-            "Docker Image Push" : {
-                sh './gradlew dockerPush'
+        stage('Build') {
+            try {
+                sh './gradlew build -x test'
+            } catch(e) {
+                mail subject: "Jenkins Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) failed with ${e.message}",
+                        to: 'codingman@outlook.kr',
+                        body: "Please go to $env.BUILD_URL."
             }
+        }
 
-        )
+        stage('Build Docker Image') {
+            def image = docker.build("spring-cloud-config", "./")
+        }
+
+//        stage('Archive') {
+//            parallel (
+//
+//                    "Archive Artifacts" : {
+//                        archiveArtifacts artifacts: '**/build/libs/*.jar', fingerprint: true
+//                    },
+//
+//                    "Docker Image Push" : {
+//                        sh './gradlew dockerPush'
+//                    }
+//
+//            )
+//        }
     }
+
 
 }
